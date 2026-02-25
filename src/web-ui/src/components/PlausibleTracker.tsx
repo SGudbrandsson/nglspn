4035699dd334b4396347b98856f355b21a5019d4
Alex Couper <amcouper@gmail.com>
2026-02-25 11:04:09 +0000

    Fix Plausible tracker SSR error by using dynamic import

--- src/web-ui/src/components/PlausibleTracker.tsx
+++ src/web-ui/src/components/PlausibleTracker.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import { useEffect } from "react";
-import { init } from "@plausible-analytics/tracker";
 
 export function PlausibleTracker() {
   useEffect(() => {
-    init({
-      domain: "naglasupan.com",
+    import("@plausible-analytics/tracker").then(({ init }) => {
+      init({
+        domain: "naglasupan.com",
+      });
     });
   }, []);
 
